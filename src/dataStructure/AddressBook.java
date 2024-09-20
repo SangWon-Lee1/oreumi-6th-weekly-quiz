da9c95499a49d786b9b342e7716bc5ce4f04b87c
Sang Won Lee <freerusya@naver.com>
2024-09-20 23:33:54 +0900

    feat: displayContacts
    Contact list output function

--- src/dataStructure/AddressBook.java
+++ src/dataStructure/AddressBook.java
@@ -13,4 +13,20 @@ public class AddressBook {
     public void addContact(Contact contact) {
         this.contacts.add(contact);
     }
+
+    public void displayContacts() {
+        if (contacts.isEmpty()) {
+            System.out.println("연락처가 비어있습니다.");
+            return;
+        }
+        for (Contact contact : contacts) {
+            if (contact instanceof BusinessContact) {
+                BusinessContact businessContact = (BusinessContact) contact;
+                System.out.println("이름: " + contact.getName() + ", 전화번호: " + contact.getPhoneNumber() + ", 회사명: " + businessContact.getCompany());
+            } else {
+                PersonalContact personalContact = (PersonalContact) contact;
+                System.out.println("이름: " + contact.getName() + ", 전화번호: " + contact.getPhoneNumber() + ", 관계: " + personalContact.getRelationship());
+            }
+        }
+    }
 }
