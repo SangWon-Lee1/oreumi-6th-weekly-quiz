489b83e34716fb894ecd2452c36393fc139fd3d2
Sang Won Lee <freerusya@naver.com>
2024-09-20 23:31:19 +0900

    feat: add AddressBook.java

---
+++ src/dataStructure/AddressBook.java
@@ -0,0 +1,12 @@
+package dataStructure;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+    List<Contact> contacts;
+
+    public AddressBook() {
+        this.contacts = new ArrayList<>();
+    }
+}
